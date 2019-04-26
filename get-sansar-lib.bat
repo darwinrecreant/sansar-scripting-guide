81811dffa5ca2faf15c76f4c307e88e42ddbe358
EvoAv <darwinrecreant@gmail.com>
2019-04-26 23:38:01 +0000

    Added example scripts

--- get-sansar-lib.bat
+++ get-sansar-lib.bat
@@ -1,5 +1,7 @@
 if not exist ".\Assemblies" mkdir ".\Assemblies"
 if not exist ".\ScriptLibrary" mkdir ".\ScriptLibrary"
+if not exist ".\Examples" mkdir ".\Examples"
 robocopy "D:\Steam\steamapps\common\Sansar\Client\ScriptApi\Assemblies" ".\Assemblies" /E
 robocopy "D:\Steam\steamapps\common\Sansar\Client\ScriptApi\Examples\ScriptLibrary" ".\ScriptLibrary" /E
+robocopy "D:\Steam\steamapps\common\Sansar\Client\ScriptApi\Examples" ".\Examples"
 pause 10
